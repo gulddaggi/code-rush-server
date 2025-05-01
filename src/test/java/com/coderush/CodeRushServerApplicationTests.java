c8138ebc964281b13e824aa41b67029c5fbd2a28
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2025-05-02 02:02:35 +0900

    프로젝트 생성

---
+++ src/test/java/com/coderush/CodeRushServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRushServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
